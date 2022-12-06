8f1f865249551d920b4d90628c43c6e23305154f
towatti1009 <64978137+towatti1009@users.noreply.github.com>
2022-12-06 10:37:05 +0900

    白黒しきい値自動チューニング機能追加(再)/ライントレース基礎部分/座標認識

---
+++ robot/asp-gr_peach_gcc-mbed-master/examples/truestudio/camtank/app.cpp
@@ -0,0 +1,256 @@
+#include <kernel.h>
+#include "kernel_cfg.h"
+#include "app.h"
+#include "mbed.h"
+#include "app_config.h"
+#include "GR_PEACH_WlanBP3595.h"
+#include "Zumo.h"
+#include "Milkcocoa.h"
+
+#define SERVER_ADDRESS "192.168.3.50"
+#define SERVER_PORT 9002
+
+//static void _wlan_inf_callback(uint8_t ucType, uint16_t usWid, uint16_t usSize, uint8_t *pucData);
+//static void error_wait(int ret, const char* str);
+
+GR_PEACH_WlanBP3595 wlan;			// Wifiルータ用
+TCPSocketConnection socket;			// TCPソケット変数
+
+// LED
+DigitalOut led_r(LED1);
+DigitalOut led_g(LED2);
+DigitalOut led_b(LED3);
+
+Serial pc(USBTX, USBRX);
+Zumo zumo;
+
+static int speed = 80;
+static int steer = 0;
+
+void ledStatus(int r, int g, int b)
+{
+	led_r = r;
+	led_g = g;
+	led_b = b;
+}
+
+void ledBlink(int r, int g, int b,int count,int interval){
+	for(int i=0;i<count;i++){
+		ledStatus(r,g,b);
+		Thread::wait(interval);
+		ledStatus(0,0,0);
+		Thread::wait(interval);
+	}
+}
+
+// WiFi Routerとの接続
+/*void WiFi_init() {
+	int ret;
+
+	wlan.setWlanCbFunction(_wlan_inf_callback);
+
+	pc.printf("\r\ninitializing\r\n");
+	ret = wlan.init(IP_ADDRESS, SUBNET_MASK, DEFAULT_GATEWAY);
+	error_wait(ret, "init");	// if error, wait
+	//緑二回点滅
+	for(int i=0;i<2;i++){
+		ledStatus(0,1,0);
+		Thread::wait(500);
+		ledStatus(0,0,0);
+		Thread::wait(500);
+	}
+	pc.printf("My addr=%s\r\n",IP_ADDRESS);
+
+	pc.printf("wlan connecting\r\n");
+	ret = wlan.connect(WLAN_SSID, WLAN_PSK);
+	error_wait(ret, "wifi connect error");	// if error, message&Red LED turn on
+	pc.printf("wlan connectted SSID=%s\r\n",WLAN_SSID);		// if no error , print this message
+	//青二回点滅
+	for(int i=0;i<2;i++){
+		ledStatus(0,0,1);
+		Thread::wait(500);
+		ledStatus(0,0,0);
+		Thread::wait(500);
+	}
+	//　　wifi routerとの接続が成功するとZUMOの青色LEDが点灯する
+}
+
+//　TCP/IP通信のセッション確立処理
+void connect_tcp_session() {
+	int ret;
+
+	ret = socket.connect(SERVER_ADDRESS, SERVER_PORT);
+	error_wait(ret, "socket connect");	//　うまくいかないときは、赤のLEDランプが点灯する
+	//青緑二回点滅
+	for(int i=0;i<2;i++){
+			ledStatus(0,1,1);
+			dly_tsk(500);
+			ledStatus(0,0,0);
+			dly_tsk(500);
+	}
+	pc.printf("Socket connected\r\n");
+	pc.printf("server IP&port= %s, %d\r\n",SERVER_ADDRESS, SERVER_PORT);	//　成功
+}
+*/
+
+void tuning_IR(int *threshold){
+	char c = 0, WorB, drop = 30;
+	unsigned int IR_values[6];//, IR_buf[6];
+
+	for(char i=0;i<6;i++){
+		threshold[i] = 0;
+	}
+
+	for(WorB=0;WorB<2;WorB++){
+		if(!WorB){
+			pc.printf("Tuning mode: white\r\n");
+			ledBlink(0,1,0,5,500);
+			ledStatus(0,1,0);
+		}else{
+			pc.printf("Tuning mode: black\r\n");
+			ledBlink(0,0,1,5,500);
+			ledStatus(0,0,1);
+		}
+		while(c<10){
+			dly_tsk(500);
+			zumo.readAnalogIrValue(IR_values);
+			//決め打ちよりバッファを取って瞬低を検知したほうが確実。リソースが足りるかどうか（あとめんどい）
+			if(IR_values[5] < drop || IR_values[3] < drop || IR_values[0] < drop){
+				pc.printf("instantaneous drop detected\r\n");
+				dly_tsk(1000);
+				continue;
+			}else{
+				pc.printf("left:%d, center:%d, right:%d\r\n", IR_values[5], IR_values[3], IR_values[0]);
+				for(char j=0;j<3;j++){
+					//5-j*2-(j/2)...0のとき5,1のとき3,2のとき0が出てくる都合のいい数式(j/2はint)
+					threshold[WorB*3+j] += IR_values[5-j*2-(j/2)];
+					pc.printf("%d\r\n",threshold[WorB*3+j]);
+				}
+				c++;
+			}
+		}
+		c=0;
+	}
+	ledStatus(0,0,0);
+
+	for(char i=0;i<6;i++){
+		threshold[i] /= 10;
+	}
+}
+
+void task_main(intptr_t exinf) {
+	pc.baud(115200);
+	pc.printf("Initializing...\r\n");
+
+	IrBitField_T irbits;
+	unsigned int IR_values[6];
+	int threshold[6];
+	char position[2] = {0,0}, rotation = 0;
+
+	while(1){
+		zumo.driveTank(0,0);
+		zumo.readAnalogIrValue(IR_values);
+		zumo.readIr(irbits);
+
+		pc.printf("left:%d %d, center:%d %d, right:%d %d\r\n", IR_values[5] ,IR[0], IR_values[3], IR[1], IR_values[0], IR[2]);
+		if((IR_values[5] <= 150) && (IR_values[3] >= 150) && (IR_values[0] <= 150)){
+			pc.printf("Drive\r\n");
+			zumo.driveTank(100,100);
+			position[0]++;
+			dly_tsk(250);
+		}else if((IR_values[5] >= 150) && (IR_values[3] >= 150) && (IR_values[0] >= 150)){
+			pc.printf("Slow\r\n");
+			zumo.driveTank(50,50);
+			dly_tsk(250);
+		}else if((IR_values[5] <= 150) && (IR_values[3] <= 150) && (IR_values[0] <= 150)){
+			pc.printf("Stop\r\n");
+			zumo.driveTank(0,0);
+			dly_tsk(250);
+		}
+	}
+	/*dly_tsk(5000);
+	tuning_IR(threshold);
+	for(int i=0;i<6;i++){
+		pc.printf("threshold[%d]:%d,\r\n",i,threshold[i]);
+	*/
+
+	/*
+	//networkの初期化
+	WiFi_init();
+	connect_tcp_session();
+
+	//文字列を送信する
+	char* data = strdup("Connection Success\r\n");
+	for (int i=0; i<20; ++i){
+		ret = socket.send(data, strlen(data)); //文字列,文字数
+		if(ret<0){
+			pc.printf("socket send error\r\n");
+			return;
+		}
+		else{
+			pc.printf("socket send %d byte\r\n", ret);
+		}
+	}
+	dly_tsk(1000); //サーバが受信し終えるのを待つ
+
+	char rstr[1];
+	while (1) {
+		ledStatus(0,1,1);
+		zumo.driveTank(0,0);
+		ret = socket.receive(rstr, 1);
+		if(ret >= 0){
+			ledStatus(1,0,1);
+			pc.printf("rcv command = %s\r\n",rstr[0]);
+			switch (rstr[0]) {
+			case 'w':
+				zumo.driveTank(speed,speed);
+				break;
+			case 'a':
+				zumo.driveTank(-speed*2,speed*2);
+				break;
+			case 's':
+				zumo.driveTank(-speed,-speed);
+				break;
+			case 'd':
+				zumo.driveTank(speed*2,-speed*2);
+				break;
+			case 'e':
+				//ソケットクローズしたときにバックするバグがある...？
+				pc.printf("socket close & disconnect\r\n");
+				socket.close();
+				wlan.disconnect();
+				ledStatus(1,1,1);
+				break;
+			default:
+				continue;
+			}
+			dly_tsk(500);
+			ret = socket.send("comp", 4);
+		}else{
+			ledStatus(1,0,0);
+		}
+	}*/
+}
+
+/*static void _wlan_inf_callback( uint8_t ucType, uint16_t usWid, uint16_t usSize, uint8_t *pucData ){
+	 if (ucType=='I'){
+		 if(usWid ==0x0005){
+			 if(pucData[0]==0x01){
+                   ledStatus(0,1,0);
+			 }else{
+				   ledStatus(1,0,1);
+			 }
+		 }
+	 }
+}
+static void error_wait(int ret, const char* str){
+	if( ret !=0 ){
+	   pc.printf(str);
+
+		ledStatus(1,0,0);
+		while(1){
+			Thread::wait(1000);
+		}
+	}
+}
+*/
---
+++ robot/asp-gr_peach_gcc-mbed-master/examples/truestudio/camtank/kernel_cfg.h
@@ -0,0 +1,32 @@
+/* kernel_cfg.h */
+#ifndef TOPPERS_KERNEL_CFG_H
+#define TOPPERS_KERNEL_CFG_H
+
+#define TNUM_TSKID	213
+#define TNUM_SEMID	209
+#define TNUM_FLGID	200
+#define TNUM_DTQID	201
+#define TNUM_PDQID	0
+#define TNUM_MBXID	0
+#define TNUM_MTXID	200
+#define TNUM_MPFID	0
+#define TNUM_CYCID	0
+#define TNUM_ALMID	0
+#define TNUM_ISRID	0
+
+#define LOGTASK	1
+#define INIT_MAIN_TASK	2
+#define TASKID_MAIN	3
+#define SERIAL_RCV_SEM1	1
+#define SERIAL_SND_SEM1	2
+#define SERIAL_RCV_SEM2	3
+#define SERIAL_SND_SEM2	4
+#define SERIAL_RCV_SEM3	5
+#define SERIAL_SND_SEM3	6
+#define SERIAL_RCV_SEM4	7
+#define SERIAL_SND_SEM4	8
+#define SEMID_SNAPSHOT	9
+#define DTQID_CHARLCD	1
+
+#endif /* TOPPERS_KERNEL_CFG_H */
+
