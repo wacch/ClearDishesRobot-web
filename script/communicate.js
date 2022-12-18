16520e4b0894271143e4ee5b112123945d6e74eb
towatti1009 <64978137+towatti1009@users.noreply.github.com>
2022-12-18 18:34:21 +0900

    不要な処理とコメント文の削除

--- script/communicate.js
+++ script/communicate.js
@@ -37,27 +37,6 @@ btn_retry.addEventListener('click', function () {
 });
 
 websocketClient();
-/*var ret = setTimeout(function(){
-    // 指定した秒数後に実行したい処理を書く
-    var num = Math.floor(Math.random()*2);
-    if(num == 0){
-        // succeed
-        console.log('succeed');
-        stat.innerHTML = '通信成功';
-        txt.innerHTML = succeed_1 + '<span id="seat">' + seat + '</span>' + succeed_2;
-        btn.innerHTML = 'OK';
-    }
-    else if(num == 1){
-        // failed
-        console.log('failed');
-        stat.innerHTML = '通信失敗';
-        txt.innerHTML = failed;
-        btn.innerHTML = 'ホームへ'
-    }
-    btn.removeEventListener('click', btn_cancel);
-    btn.addEventListener('click', btn_go_home);
-}, 5000);*/
-
 
 function setDefault() {
     ack = fin = false;
@@ -82,20 +61,6 @@ function setSucceed() {
     txt.innerHTML = msg_succeed_1 + '<span id="seat">' + seat + '</span>' + msg_succeed_2;
     btn.style.visibility = 'visible'
 }
-
-/*function btn_go_home() {
-    window.location = 'index.html';
-}
-function btn_cancel() {
-    clearTimeout(ret);
-    console.log('cancel');
-    stat.innerHTML = '通信中断';
-    txt.innerHTML = msg_suspension;
-    btn.innerHTML = 'ホームへ'
-    this.removeEventListener('click', btn_cancel);
-    this.addEventListener('click', btn_go_home);
-}
-btn.addEventListener('click', btn_cancel);*/
 
 function websocketClient() {
     var ws = new WebSocket(ip_addr);
@@ -135,24 +100,4 @@ function websocketClient() {
         if(!fin)
             setFailed('connection failure; cannot connect to \'' + ip_addr + '\'');
     };
-}
-
-/*function Responce(msg = '') {
-    if (!ack && !err) {
-        lock = true;
-        setFailed(msg);
-    } else if (err) {
-        setFailed(msg);
-    } else if (ack && !lock && !err) {
-        setSucceed();
-    }
-    btn.style.visibility = 'visible'
-    btn_retry.style.visibility = 'visible'
-}*/
-
-function sleep(waitMsec) {
-    var startMsec = new Date();
-
-    // 指定ミリ秒間だけループさせる（CPUは常にビジー状態）
-    while (new Date() - startMsec < waitMsec);
 }
